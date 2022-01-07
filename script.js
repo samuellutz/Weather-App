b2471c47db4287994a8e52daaabfcd50b7a64af6
samuel lutz <samlutz89@gmail.com>
2022-01-07 11:08:21 -0800

    initial javascript for typing in location

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-$(document).ready(function() {
-    function weather() {
-        var url = "api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid=1506795a9042438dd4635266f5cbd0eb";
-        $.getJSON(url, function(data) {
-            console.log(data)
-        })
-    }
-});
+function searchWeather(x) {
+    $.ajax({
+      type: "GET",
+      url: "http://api.openweathermap.org/data/2.5/forecast?q= + x +&appid=1506795a9042438dd4635266f5cbd0eb",
+      dataType: "json",
+      // error handling
+      error: function(e){
+          alert("Error-Connection");
+      }
+    // runs whenever call goes through
+    }).then(function(response){
+        console.log(response);
+        console.log(reponse.name)
+
+    })
+  }
+
+//   var searchValue = "Austin";
+//   searchWeather(searchValue);
+
+  $("#search").on("click", function(event){
+    event.preventDefault();
+    var input = $("#city").val();
+    console.log("input: " + input);
+    searchWeather(input);
+  });
+
+  function create(x){
+      var newLi = $("<li>");
+      newLi.text(x);
+      $("#cities").append(newLi);
+  } 
