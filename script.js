94d771bdcda6daf43f4816eda9381e9e7a932333
samuel lutz <samlutz89@gmail.com>
2022-01-08 08:43:46 -0800

    weather outputs properly

--- script.js
+++ script.js
@@ -1,50 +1,80 @@
-var container = []
+var container = [];
 
 
 function searchWeather(x) {
-    $.ajax({
-      type: "GET",
-      url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + x + '&appid=1506795a9042438dd4635266f5cbd0eb',
-      dataType: "json",
-      // error handling
-      error: function(e){
-          alert("Error-Connection");
-      }
-    // runs whenever call goes through
-    }).then(function(response){
-        console.log(response);
-        console.log(response.name)
-
-        $("#cityName").text("City: " + response.name);
-        var temp = $("<p>");
-        temp.text("Temperature: " + response.main.temp + " F");
-        $("#cityInfo").append(temp);
-        var humidity = $("<p>");
-        humidity.text("humidity: " + response.main.humidity + " %");
-        $("#cityInfo").append(humidity);
-        var wind = $("<p>");
-        wind.text("Temperature: " + response.wind.speed + " MPH");
-        $("#cityInfo").append(wind);
-        var uv = $("<p>");
-        uv.text("UV");
-        $("#cityInfo").append(uv);
-
-    })
-  }
-
-// should search city.
-
-  $("#search").on("click", function(event){
-    event.preventDefault();
-    var input = $("#city").val();
-    console.log("input: " + input);
-    searchWeather(input);
-  });
-// to make the list of cities
+  $.ajax({
+    type: "GET",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + x + "&appid=ebdcfef8b5f11a300f888ec06cffdf1c&units=imperial",
+    dataType: "json",
+    // error handling
+    error: function(e){
+        alert("Error-Connection");
+    }
+  // outputs weather data to the city section
+  }).then(function(response){
+      console.log(response);
+      console.log(response.name)
+      $("#cityName").text("City: " + response.name);
+      var temp = $("<p>");
+      temp.text("Temperature: " + response.main.temp + " F");
+      $("#cityInfo").append(temp);
+      var humidity = $("<p>");
+      humidity.text("humidity: " + response.main.humidity + " %");
+      $("#cityInfo").append(humidity);
+      var wind = $("<p>");
+      wind.text("Temperature: " + response.wind.speed + " MPH");
+      $("#cityInfo").append(wind);
+      var uv = $("<p>");
+      uv.text("UV");
+      $("#cityInfo").append(uv);
+      
+  })
+}
+// searches city
+$("#search").on("click", function(event){
+  event.preventDefault();
+  var input = $("#city").val();
+  console.log("input: " + input);
+  searchWeather(input);
+});
 
 function addCity(x){
-      var newLi = document.createElement("button");
-      newLi.text(x);
-      $("#Cities").add.text("addCity");
-  addCity();
+    var newLi = $("<li>");
+    // newLi.text(x);
+    $("#cities").append(newLi);
+    var button = $("<button>");
+    button.text(x);
+    button.addClass("btn btn-secondary cityButton mb-2");
+    newLi.append(button);
+    container.push(x);
+    console.log(container);
+    for(var i = 0; i < container.length; i++){
+    localStorage.setItem(i, container[i]);
   }
+  localStorage.setItem("Number", container.length);
+}
+
+$(".cityButton").on("click", function(event){
+  event.preventDefault();
+  var btnVal = this.text;
+  console.log(btnVal);
+
+});
+
+function pageOpen(){
+  var num = localStorage.getItem("Number");
+  for(var i = 0; i < num; i++){
+    container.push(localStorage.getItem(i));
+  }
+
+  for(var i = 0; i < container.length; i++){
+    var li = $("<li>");
+    // newLi.text(x);
+    $("#cities").append(li);
+    var ogButton = $("<button>");
+    ogButton.text(container[i]);
+    ogButton.addClass("btn btn-secondary cityButton mb-2");
+    li.append(ogButton);
+  }
+  console.log(container);
+}
